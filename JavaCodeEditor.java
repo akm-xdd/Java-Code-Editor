7f5af7bd3e3047f4eb11137d43162dc01440f949
Anand Kumar Mishra <110248822+akm-xdd@users.noreply.github.com>
2023-01-12 20:30:03 +0530

    Add files via upload

--- JavaCodeEditor.java
+++ JavaCodeEditor.java
@@ -1,17 +1,25 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.Element;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.io.*;
+
 
 public class JavaCodeEditor extends JFrame {
 
-    private JTextArea textArea;
+    private final JTextArea textArea;
+    private final JTextArea lineNumberArea;
+    private final UndoManager undo = new UndoManager();
+    private final UndoAction undoAction = new UndoAction();
+    private final RedoAction redoAction = new RedoAction();
     private JFileChooser fileChooser;
-
-    private JTextArea lineNumberArea;
 
     public JavaCodeEditor() {
         // Set the title and layout of the frame
@@ -21,30 +29,37 @@ public class JavaCodeEditor extends JFrame {
         setLayout(new BorderLayout());
 
         // Create the text area and add it to the frame
-        textArea = new JTextArea();
+        textArea = new JTextArea("//THIS IS PRISTINE Start writing your code below this line");
+        textArea.setFont(textArea.getFont().deriveFont(Font.BOLD));
+
         add(new JScrollPane(textArea), BorderLayout.CENTER);
         lineNumberArea = new JTextArea("1");
         lineNumberArea.setBackground(Color.LIGHT_GRAY);
         lineNumberArea.setEditable(false);
+
+        // Create a new document filter
+
+
         textArea.getDocument().addDocumentListener(new DocumentListener() {
-            public String getText(){
-                int caretPosition = textArea.getDocument().getLength();
+            public String getText() {
                 Element root = textArea.getDocument().getDefaultRootElement();
-                String text = "1" + System.getProperty("line.separator");
-                for(int i = 2; i < root.getElementCount() + 1; i++){
-                    text += i + System.getProperty("line.separator");
+                StringBuilder text = new StringBuilder("1" + System.getProperty("line.separator"));
+                for (int i = 2; i < root.getElementCount() + 1; i++) {
+                    text.append(i).append(System.getProperty("line.separator"));
                 }
-                return text;
+                return text.toString();
             }
 
             @Override
             public void changedUpdate(DocumentEvent e) {
                 lineNumberArea.setText(getText());
             }
+
             @Override
             public void insertUpdate(DocumentEvent e) {
                 lineNumberArea.setText(getText());
             }
+
             @Override
             public void removeUpdate(DocumentEvent e) {
                 lineNumberArea.setText(getText());
@@ -69,33 +84,23 @@ public class JavaCodeEditor extends JFrame {
         fileMenu.add(saveItem);
 
         // Add an ActionListener to the Open menu item
-        openItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                openFile();
-            }
-        });
+        openItem.addActionListener(e -> openFile());
 
         // Add an ActionListener to the Save menu item
-        saveItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                saveFile();
-            }
-        });
+        saveItem.addActionListener(e -> saveFile());
 
         // Create the Edit menu and add it to the menu bar
         JMenu editMenu = new JMenu("Edit");
         menuBar.add(editMenu);
 
+
         // Create the Find and Replace menu item and add it to the Edit menu
         JMenuItem findAndReplaceItem = new JMenuItem("Find and Replace");
         editMenu.add(findAndReplaceItem);
 
+
         // Add an ActionListener to the Find and Replace menu item
-        findAndReplaceItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                findAndReplace();
-            }
-        });
+        findAndReplaceItem.addActionListener(e -> findAndReplace());
 
         // Create the About menu and add it to the menu bar
         JMenu aboutMenu = new JMenu("More");
@@ -109,33 +114,50 @@ public class JavaCodeEditor extends JFrame {
         aboutMenu.add(switchTheme);
 
         final boolean[] isDark = {false};
-        switchTheme.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (isDark[0]) {
-                    textArea.setBackground(Color.WHITE);
-                    textArea.setForeground(Color.BLACK);
-                    isDark[0] = false;
-                } else {
-                    textArea.setBackground(Color.DARK_GRAY);
-                    textArea.setForeground(Color.WHITE);
-                    isDark[0] = true;
-                }
+        switchTheme.addActionListener(e -> {
+            if (isDark[0]) {
+                textArea.setBackground(Color.WHITE);
+                textArea.setForeground(Color.BLACK);
+                isDark[0] = false;
+            } else {
+                textArea.setBackground(Color.DARK_GRAY);
+                textArea.setForeground(Color.WHITE);
+                isDark[0] = true;
             }
         });
 
         // Add an ActionListener to the About menu item
-        aboutItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                about();
-            }
-        });
+        aboutItem.addActionListener(e -> about());
 
         // set the default close operation and size of the frame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1920, 1080);
 
+        textArea.getDocument().addUndoableEditListener(e -> undo.addEdit(e.getEdit()));
+        InputMap im = textArea.getInputMap(JComponent.WHEN_FOCUSED);
+        ActionMap am = textArea.getActionMap();
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK), "undo");
+        am.put("undo", undoAction);
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.CTRL_DOWN_MASK), "redo");
+        am.put("redo", redoAction);
+        textArea.addMouseWheelListener(e -> {
+            if (e.isControlDown()) {
+                int zoom = e.getWheelRotation();
+                if (zoom > 0) {
+                    textArea.setFont(textArea.getFont().deriveFont(textArea.getFont().getSize() - 2f));
+                    lineNumberArea.setFont(lineNumberArea.getFont().deriveFont(lineNumberArea.getFont().getSize() - 2f));
+                } else {
+                    textArea.setFont(textArea.getFont().deriveFont(textArea.getFont().getSize() + 2f));
+                    lineNumberArea.setFont(lineNumberArea.getFont().deriveFont(lineNumberArea.getFont().getSize() + 2f));
+                }
+            }
+        });
 
+    }
+
+    public static void main(String[] args) {
+        JavaCodeEditor editor = new JavaCodeEditor();
+        editor.setVisible(true);
     }
 
     private void openFile() {
@@ -173,8 +195,8 @@ public class JavaCodeEditor extends JFrame {
                 JOptionPane.showMessageDialog(this, "Error reading file", "Error", JOptionPane.ERROR_MESSAGE);
             }
         }
-    }
 
+    }
 
     private void saveFile() {
         // Create a file chooser
@@ -213,7 +235,6 @@ public class JavaCodeEditor extends JFrame {
         }
     }
 
-
     private void findAndReplace() {
         String findText = JOptionPane.showInputDialog(this, "Enter the text to find:");
         if (findText == null) {
@@ -232,16 +253,40 @@ public class JavaCodeEditor extends JFrame {
             index = textArea.getText().indexOf(findText, index + replaceText.length());
         }
 
-        JOptionPane.showOptionDialog(this, "We made " + count + " replacements!", "Results", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[] {}, null);
+        JOptionPane.showOptionDialog(this, "We made " + count + " replacements!", "Results", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{}, null);
     }
-    
+
     private void about() {
         JOptionPane.showMessageDialog(this, "THIS IS PRISTINE!!!", "About", JOptionPane.INFORMATION_MESSAGE);
 
     }
 
-    public static void main(String[] args) {
-        JavaCodeEditor editor = new JavaCodeEditor();
-        editor.setVisible(true);
+    protected void update() {
+        undoAction.setEnabled(undo.canUndo());
+        redoAction.setEnabled(undo.canRedo());
+    }
+
+    private class UndoAction extends AbstractAction {
+        public void actionPerformed(ActionEvent e) {
+            try {
+                undo.undo();
+            } catch (CannotUndoException ex) {
+                // Handle exception
+            }
+            update();
+        }
+    }
+
+    private class RedoAction extends AbstractAction {
+        public void actionPerformed(ActionEvent e) {
+            try {
+                undo.redo();
+            } catch (CannotRedoException ex) {
+                // Handle exception
+            }
+            update();
+        }
+
     }
 }
+
